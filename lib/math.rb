d22cb30c2245459636df7a3a97266e7b41bfb317
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-09 23:57:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,22 @@ end
 addition(1, 2)
 
 def subtraction(num1, num2)
-  num1 - num2
+  puts num1 - num2
 end
-
+subtraction(5,5)
 def division(num1, num2)
-    num1 / num2
+    puts num1 / num2
 end
-
+division(1,2)
 def multiplication(num1, num2)
-    num1 * num2
+    puts num1 * num2
 end
-
+multiplication(5,2)
 def modulo(num1, num2)
-    num1 % num2
+    puts num1 % num2
 end
-
+modulo(2,4)
 def square_root(num)
-    Math.sqrt(num)
+    puts Math.sqrt(num)
 end
+square_root(16)
